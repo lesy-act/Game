d3f75bcc116a3711747054e918f175ff986233ef
admin <admin@sylv.local>
2026-09-11 23:21:55 +0700

    Create App module

---
+++ build/CMakeFiles/SDL2Demo.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SDL2Demo.dir/src/App.cpp.o"
+  "CMakeFiles/SDL2Demo.dir/src/App.cpp.o.d"
+  "CMakeFiles/SDL2Demo.dir/src/main.cpp.o"
+  "CMakeFiles/SDL2Demo.dir/src/main.cpp.o.d"
+  "SDL2Demo"
+  "SDL2Demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SDL2Demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
